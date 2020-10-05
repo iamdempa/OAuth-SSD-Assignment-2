d7a749d4248e6f2220a636dc91f03e924972325d
iamdempa <banukajananathjayarathna@gmail.com>
2020-10-05 06:19:10 +0530

    test

--- dist/calendarevents.657aa765.js
+++ dist/calendarevents.657aa765.js
@@ -46218,7 +46218,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "33379" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40121" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
